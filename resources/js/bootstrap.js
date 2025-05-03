b38a46ebac8e0d339ee599679f033d328a75ad46
Ngozistephen <ngozi.stephen99@gmail.com>
2025-05-03 14:45:51 +0100

    making change in the websocket 2

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -25,8 +25,9 @@ window.Echo = new Echo({
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     wsHost: import.meta.env.VITE_PUSHER_HOST || window.location.hostname,
-    wsPort: import.meta.env.VITE_PUSHER_PORT || 6001,
-    wssPort: import.meta.env.VITE_PUSHER_PORT || 6001,
+    wsPath: '/laravel-websockets',
+    wsPort: import.meta.env.VITE_PUSHER_PORT || 443,
+    wssPort: import.meta.env.VITE_PUSHER_PORT || 443,
     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME || 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
 });
