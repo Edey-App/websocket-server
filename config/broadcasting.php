444a94825383b2246a6eb1d878e5b6a312910da7
Ngozistephen <ngozi.stephen99@gmail.com>
2025-05-03 17:32:18 +0100

    making change in the websocket 7

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,7 +38,7 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'host' => env('PUSHER_HOST', 'websocket.inhouse.codes'),
-                'port' => env('PUSHER_PORT', 443),
+                'port' => env('PUSHER_PORT'),
                 'scheme' => env('PUSHER_SCHEME', 'https'),
                 'encrypted' => true,
                 'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
